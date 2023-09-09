a27a261a2d155f3eb94701f0a923a55fb2625038
tompaw <gurtoc@gmail.com>
2023-09-09 16:26:32 +0200

    added nav-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FrontPageComponent } from './front-page/front-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './footer/footer.component';
 import { DockerDesktopComponent } from './front-page/docker-desktop/docker-desktop.component';
+import { BusinessCustomerComponent } from './front-page/business-customer/business-customer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { DockerDesktopComponent } from './front-page/docker-desktop/docker-deskt
     FrontPageComponent,
     FooterComponent,
     DockerDesktopComponent,
+    BusinessCustomerComponent,
+    NavBarComponent,
 
   ],
   imports: [
---
+++ src/app/front-page/business-customer/business-customer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-business-customer',
+  templateUrl: './business-customer.component.html',
+  styleUrls: ['./business-customer.component.css']
+})
+export class BusinessCustomerComponent {
+
+}
